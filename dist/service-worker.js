419126e7fd318cb9e8a9b1912773ac80acd3b870
Wojciech Wroblewski <wroblewskiwojciech161@gmail.com>
2022-01-15 17:10:07 +0100

    updated config

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f30458052fda2e5d535029ea3559260.js"
+  "/precache-manifest.a7682eca4dd5f7d51faed1301936519b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "thesis-defence"});
